54d3bfff0b9f4e89892ff848043c450faa636a78
JM Arenas <rns.jey@gmail.com>
2022-03-04 16:14:20 +0800

    Modify routes for each users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
   
   devise_for :users
-  mount RailsAdmin::Engine => '/super_admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/command_center/super_admin', as: 'rails_admin'
+
+  authenticated :user do
+    get '/admin', to: 'admin#index', as: 'root_admin'
+  end
   
-  root :to => "home#index"
+  root :to => "member#index"
 end
