cbe596be86fe0c9c5191322575e95e6dbba7d0f5
JM Arenas <rns.jey@gmail.com>
2022-03-04 16:14:48 +0800

    Set after sign in path for each user roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    # Here you can write logic based on roles to return different after sign in paths
+    if current_user.role == "super_admin"
+      rails_admin_path
+    elsif current_user.role == "admin"
+      root_admin_path
+    else
+      root_path
+    end
+  end
 end
