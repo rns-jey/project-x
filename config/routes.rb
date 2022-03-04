bda2d3df6d91701985c788c3b170a130953c82cc
JM Arenas <rns.jey@gmail.com>
2022-03-04 14:17:37 +0800

    Generate homepage and set it as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  get 'home/index'
+  
+  devise_for :users
   mount RailsAdmin::Engine => '/super_admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root :to => "home#index"
 end
